7b864f4db0f04b7dc5f6186b0cbb7fb3749a9206
Mindaugas Jusis <bekraujis@gmail.com>
2018-06-12 11:00:37 +0300

    Multitasking

---
+++ Looper.cpp
@@ -0,0 +1,24 @@
+#include "Arduino.h"
+#include "Looper.h"
+
+Looper::Looper() {
+  this->interval = 0;
+  this->lastCalled = millis();
+}
+
+void Looper::setInterval(unsigned long interval, void (*callback)()) {
+  this->interval = interval;
+  this->callback = callback;
+}
+
+void Looper::update() {
+  if (this->interval > 0) {
+    if ( millis() - this->lastCalled >= this->interval ) {
+      this->callback();
+      this->lastCalled += this->interval;
+    }
+  }
+}
+
+
+
---
+++ Looper.h
@@ -0,0 +1,18 @@
+#ifndef Looper_h
+#define Looper_h
+
+#include "Arduino.h"
+
+class Looper {
+  public:
+    Looper();
+    void setInterval(unsigned long, void (*)());
+    void update();
+  private:
+    void (*callback)();
+    unsigned long interval;
+    unsigned long lastCalled;
+};
+
+#endif
+
