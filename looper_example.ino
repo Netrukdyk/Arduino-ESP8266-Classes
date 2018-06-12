af7d72be9689b6c4655ce5b8106678351e0b887d
Mindaugas Jusis <bekraujis@gmail.com>
2018-06-12 11:10:05 +0300

    Example

---
+++ looper_example.ino
@@ -0,0 +1,26 @@
+// Looper example
+
+#include "Looper.h"
+
+Looper loop1;
+Looper loop2;
+Looper loop3;
+
+void setup(){
+  loop1.setInterval(1000, myFunction1);
+  loop2.setInterval(200, myFunction2);
+  loop3.setInterval(5000, myFunction3);
+}
+
+void loop(){
+}
+
+void myFunction1(){
+
+}
+void myFunction2(){
+
+}
+void myFunction3(){
+
+}
